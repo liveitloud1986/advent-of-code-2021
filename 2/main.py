079d332be78efc50326e14fc0f9f77b9771ecae2
daniel williams <daniel.williams@safely.com>
2021-12-01 19:01:59 -0500

    number two for Day One

---
+++ 2/main.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def get_three(lines):
+    for i in range(0, len(lines), 1):
+        yield lines[i:i+3]
+
+try:
+    with open('data.txt', 'r') as f:
+        lines = f.readlines()
+        lines = [l.strip('\n') for l in lines]
+
+        num_increases = 0
+        list_of_threes = []
+        for three_lines in get_three(lines):
+            list_of_threes.append(three_lines)
+
+        for index, three_lines in enumerate(list_of_threes):
+            if index > 0:
+                if int(sum(int(l) for l in list_of_threes[index - 1])) < sum(int(l) for l in three_lines):
+                    num_increases += 1
+
+        print(num_increases)
+except Exception as e:
+    print(e)
